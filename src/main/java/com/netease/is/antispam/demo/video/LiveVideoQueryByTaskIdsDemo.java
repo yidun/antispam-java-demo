4a07a2c9371afba189f30f057a94c6ceb2fad549
白建伟 <baijianwei@corp.netease.com>
2019-09-10 17:52:29 +0800

    直播视频根据taskId查询接口demo代码事例

---
+++ src/main/java/com/netease/is/antispam/demo/video/LiveVideoQueryByTaskIdsDemo.java
@@ -0,0 +1,95 @@
+/*
+ * @(#) TextCallbackAPIDemo.java 2016年12月28日
+ * 
+ * Copyright 2010 NetEase.com, Inc. All rights reserved.
+ */
+package com.netease.is.antispam.demo.video;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import org.apache.http.Consts;
+import org.apache.http.client.HttpClient;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.netease.is.antispam.demo.utils.HttpClient4Utils;
+import com.netease.is.antispam.demo.utils.SignatureUtils;
+
+/**
+ * 调用易盾反垃圾云服务直播视频结果查询接口API示例，该示例依赖以下jar包： 1. httpclient，用于发送http请求 2. commons-codec，使用md5算法生成签名信息，详细见SignatureUtils.java
+ * 3. gson，用于做json解析
+ * 
+ * @author habaijianwei
+ * @version 2019年09月10日
+ */
+public class LiveVideoQueryByTaskIdsDemo {
+
+    /** 产品密钥ID，产品标识 */
+    private final static String SECRETID = "your_secret_id";
+    /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
+    private final static String SECRETKEY = "your_secret_key";
+    /** 业务ID，易盾根据产品业务特点分配 */
+    private final static String BUSINESSID = "your_business_id";
+    /** 易盾反垃圾云服务点播查询检测结果获取接口地址 */
+    private final static String API_URL = "https://as.dun.163yun.com/v1/livevideo/query/task";
+    /** 实例化HttpClient，发送http请求使用，可根据需要自行调参 */
+    private static HttpClient httpClient = HttpClient4Utils.createHttpClient(100, 20, 10000, 2000, 2000);
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Map<String, String> params = new HashMap<String, String>();
+        // 1.设置公共参数
+        params.put("secretId", SECRETID);
+        params.put("businessId", BUSINESSID);
+        params.put("version", "v1");
+        params.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        params.put("nonce", String.valueOf(new Random().nextInt()));
+
+        // 2.设置私有参数
+        Set<String> taskIds = new HashSet<String>();
+        taskIds.add("c679d93d4a8d411cbe3454214d4b1fd7");
+        taskIds.add("49800dc7877f4b2a9d2e1dec92b988b6");
+        params.put("taskIds", new Gson().toJson(taskIds));
+
+        // 3.生成签名信息
+        String signature = SignatureUtils.genSignature(SECRETKEY, params);
+        params.put("signature", signature);
+
+        // 4.发送HTTP请求，这里使用的是HttpClient工具包，产品可自行选择自己熟悉的工具包发送请求
+        String response = HttpClient4Utils.sendPost(httpClient, API_URL, params, Consts.UTF_8);
+
+        // 5.解析接口返回值
+        JsonObject resultObject = new JsonParser().parse(response).getAsJsonObject();
+        int code = resultObject.get("code").getAsInt();
+        String msg = resultObject.get("msg").getAsString();
+        if (code == 200) {
+            JsonArray resultArray = resultObject.getAsJsonArray("result");
+            for (JsonElement jsonElement : resultArray) {
+                JsonObject jObject = jsonElement.getAsJsonObject();
+                // 直播视频uuid
+                String taskId = jObject.get("taskId").getAsString();
+                // 直播状态, 101:直播中，102：直播结束
+                int status = jObject.get("status").getAsInt();
+                // 回调标识
+                String callback = jObject.get("callback").getAsString();
+                // 直播检测状态(0:检测成功，10：检测中，110：请求重复，120：参数错误，130：解析错误，140：数据类型错误，150：并发超限)
+                int callbackStatus = jObject.get("callbackStatus").getAsInt();
+                // 过期状态（20:直播不是七天内的数据，30：直播taskId不存在）
+                int expireStatus = jObject.get("expireStatus").getAsInt();
+            }
+        } else {
+            System.out.println(String.format("ERROR: code=%s, msg=%s", code, msg));
+        }
+    }
+}
