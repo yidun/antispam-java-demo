7bc32561b69501a89bf7728a11ae423b396cdc0b
maxiaofeng <maxiaofeng@corp.netease.com>
2020-09-07 11:41:13 +0800

    升级点播语音3.3版本

--- src/main/java/com/netease/is/antispam/demo/audio/AudioCallbackAPIDemo.java
+++ src/main/java/com/netease/is/antispam/demo/audio/AudioCallbackAPIDemo.java
@@ -58,7 +58,7 @@ public class AudioCallbackAPIDemo {
         params.put("secretId", SECRETID);
         params.put("businessId", BUSINESSID);
         // 点播语音版本v3.2及以上二级细分类结构进行调整
-        params.put("version", "v3.2");
+        params.put("version", "v3.3");
         params.put("timestamp", String.valueOf(System.currentTimeMillis()));
         params.put("nonce", String.valueOf(new Random().nextInt()));
 
