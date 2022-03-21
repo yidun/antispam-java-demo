aef712c810db1b572560052a18267cca0b9af6ba
longfan <longfan@corp.netease.com>
2022-03-21 15:58:44 +0800

    政务线内容风险查询java-demo

--- src/main/java/com/netease/is/antispam/demo/utils/HttpClient4Utils.java
+++ src/main/java/com/netease/is/antispam/demo/utils/HttpClient4Utils.java
@@ -9,15 +9,21 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -26,12 +32,14 @@ import org.apache.http.util.EntityUtils;
 
 /**
  * HttpClient工具类
+ * 
  * @author hzgaomin
  * @version 2016年2月3日
  */
 public class HttpClient4Utils {
     /**
      * 实例化HttpClient
+     * 
      * @param maxTotal
      * @param maxPerRoute
      * @param socketTimeout
@@ -40,20 +48,21 @@ public class HttpClient4Utils {
      * @return
      */
     public static HttpClient createHttpClient(int maxTotal, int maxPerRoute, int socketTimeout, int connectTimeout,
-                    int connectionRequestTimeout) {
+            int connectionRequestTimeout) {
         RequestConfig defaultRequestConfig = RequestConfig.custom().setSocketTimeout(socketTimeout)
-                        .setConnectTimeout(connectTimeout).setConnectionRequestTimeout(connectionRequestTimeout).build();
+                .setConnectTimeout(connectTimeout).setConnectionRequestTimeout(connectionRequestTimeout).build();
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(maxTotal);
         cm.setDefaultMaxPerRoute(maxPerRoute);
         CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm)
-                        .setDefaultRequestConfig(defaultRequestConfig).build();
+                .setDefaultRequestConfig(defaultRequestConfig).build();
         return httpClient;
     }
 
     /**
      * 发送post请求
-     * @param httpClient 
+     * 
+     * @param httpClient
      * @param url 请求地址
      * @param params 请求参数
      * @param encoding 编码
@@ -90,5 +99,56 @@ public class HttpClient4Utils {
             }
         }
         return resp;
+    }
+
+    public static String sendGetByHeader(HttpClient httpClient, String url, Map<String, String> params,
+            Map<String, String> headers) throws Exception {
+        HttpGet httpGet = null;
+        if (MapUtils.isNotEmpty(params)) {
+            final List<NameValuePair> qparams = new LinkedList<NameValuePair>();
+            for (Map.Entry<String, String> param : params.entrySet()) {
+                qparams.add(new BasicNameValuePair(param.getKey(), param.getValue()));
+            }
+            httpGet = new HttpGet(url + "?" + URLEncodedUtils.format(qparams, "UTF-8"));
+        } else {
+            httpGet = new HttpGet(url);
+        }
+        if (headers != null) {
+            for (String key : headers.keySet()) {
+                httpGet.addHeader(key, headers.get(key));
+            }
+        }
+        CloseableHttpResponse response = null;
+        try {
+            response = (CloseableHttpResponse) httpClient.execute(httpGet);
+            int status = response.getStatusLine().getStatusCode();
+            if (status != HttpStatus.SC_OK) {
+                // 返回状态错误，直接抛异常，记录时精简一下log输出
+                throw new IllegalStateException(String.format("错误的接口返回状态值, status=%d, response=%s", status,
+                        StringUtils.abbreviate(EntityUtils.toString(response.getEntity()), 1024)));
+            }
+            // EntityUtils is not recommended
+            return EntityUtils.toString(response.getEntity(), Charset.forName("UTF-8")); // 该方法会释放连接。。。
+        } catch (Exception e) {
+            String msg = String.format("http接口调用出错url=%s, params=%s", url, params);
+            throw new Exception(msg, e);
+        } finally {
+            closeQuietly(response);
+        }
+    }
+
+    /**
+     * 关闭response
+     *
+     * @param response
+     */
+    public static void closeQuietly(CloseableHttpResponse response) {
+        if (response != null) {
+            try {
+                response.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
