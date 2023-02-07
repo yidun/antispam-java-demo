11781de088d7329482d3784666677c34345aebb5
zhaojipu <zhaojipu@corp.netease.com>
2023-02-07 16:19:59 +0800

    图片结果反馈接口，自定义用户名单增删改查接口demo补充

---
+++ src/main/java/com/netease/is/antispam/demo/image/ImageFeedBackAPIDemo.java
@@ -0,0 +1,106 @@
+/*
+ * @(#) ImageCheckAPIDemo.java 2016年3月15日
+ *
+ * Copyright 2010 NetEase.com, Inc. All rights reserved.
+ */
+package com.netease.is.antispam.demo.image;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.netease.is.antispam.demo.utils.HttpClient4Utils;
+import com.netease.is.antispam.demo.utils.SignatureUtils;
+import com.netease.is.antispam.demo.utils.Utils;
+import org.apache.http.Consts;
+import org.apache.http.client.HttpClient;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * 调用易盾反垃圾云服务图片在线检测接口API示例，该示例依赖以下jar包： 1. httpclient，用于发送http请求 2. commons-codec，使用md5算法生成签名信息，详细见SignatureUtils.java
+ * 3. gson，用于做json解析
+ *
+ * 易盾反垃圾云服务图片结果反馈demo
+ *
+ * @author zhaojipu
+ * @version 2023年2月7日
+ */
+public class ImageFeedBackAPIDemo {
+    /**
+     * 产品密钥ID，产品标识
+     */
+    private final static String SECRETID = "your_secret_id";
+    /**
+     * 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
+     */
+    private final static String SECRETKEY = "your_secret_key";
+    /**
+     * 业务ID，易盾根据产品业务特点分配
+     */
+    private final static String BUSINESSID = "your_business_id";
+    /**
+     * 易盾反垃圾云服务图片结果反馈接口地址
+     */
+    private final static String API_URL = "http://as.dun.163.com/v2/image/feedback";
+    /**
+     * 实例化HttpClient，发送http请求使用，可根据需要自行调参
+     */
+    private static HttpClient httpClient = HttpClient4Utils.createHttpClient(100, 20, 10000, 2000, 2000);
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        // 1.设置公共参数
+        params.put("secretId", SECRETID);
+        params.put("businessId", BUSINESSID);
+        params.put("version", "v2");
+        params.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        params.put("nonce", String.valueOf(new Random().nextInt()));
+        params.put("signatureMethod", "MD5"); // MD5, SM3, SHA1, SHA256
+
+        // 2.设置私有参数
+        JsonArray jsonArray = new JsonArray();
+        // 实际反馈的图片检测数据
+        JsonObject feedBackImage = new JsonObject();
+        feedBackImage.addProperty("taskId", "图片检测接口返回的数据唯一标识");
+        // 设置图片数据级别，0：正常（通过），2：确定（不通过）
+        feedBackImage.addProperty("level", 2);
+        // 设置图片的垃圾类别
+        feedBackImage.addProperty("label", 100);
+        jsonArray.add(feedBackImage);
+
+        params.put("feedbacks", jsonArray.toString());
+
+        // 3.生成签名信息
+        // 预处理参数
+        params = Utils.pretreatmentParams(params);
+        String signature = SignatureUtils.genSignature(SECRETKEY, params);
+        params.put("signature", signature);
+
+        // 4.发送HTTP请求，这里使用的是HttpClient工具包，产品可自行选择自己熟悉的工具包发送请求
+        String response = HttpClient4Utils.sendPost(httpClient, API_URL, params, Consts.UTF_8);
+
+        // 5.解析接口返回值
+        JsonObject resultObject = new JsonParser().parse(response).getAsJsonObject();
+        int code = resultObject.get("code").getAsInt();
+        String msg = resultObject.get("msg").getAsString();
+        if (code == 200) {
+            JsonArray resultArray = resultObject.getAsJsonArray("result");
+            for (JsonElement jsonElement : resultArray) {
+                JsonObject jObject = jsonElement.getAsJsonObject();
+                String taskId = jObject.get("taskId").getAsString();
+                Integer resultCode = jObject.get("result").getAsInt();
+                System.out.println(String.format("图片结果反馈返回 taskId=%s，resultCode:%s", taskId, resultCode));
+            }
+        } else {
+            System.out.printf("ERROR: code=%s, msg=%s%n", code, msg);
+        }
+
+    }
+}
