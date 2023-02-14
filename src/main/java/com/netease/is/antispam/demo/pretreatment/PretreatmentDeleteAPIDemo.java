af5b340a8ddfddf7394efb7208f7ecdd46aeb58d
zhaojipu <zhaojipu@corp.netease.com>
2023-02-14 11:49:39 +0800

    忽略词增删改查接口demo补充

---
+++ src/main/java/com/netease/is/antispam/demo/pretreatment/PretreatmentDeleteAPIDemo.java
@@ -0,0 +1,89 @@
+/*
+ * @(#) ListSubmitAPIDemo.java 2020-01-02
+ *
+ * Copyright 2020 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.netease.is.antispam.demo.pretreatment;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.netease.is.antispam.demo.utils.HttpClient4Utils;
+import com.netease.is.antispam.demo.utils.SignatureUtils;
+import com.netease.is.antispam.demo.utils.Utils;
+import org.apache.http.Consts;
+import org.apache.http.client.HttpClient;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * 调用易盾反垃圾云服务名单批量提交接口API示例，该示例依赖以下jar包：
+ * 1. httpclient，用于发送http请求
+ * 2. commons-codec，使用md5算法生成签名信息，详细见SignatureUtils.java
+ * 3. gson，用于做json解析
+ *
+ * @author zhaojipu
+ * @version 2023年02月13日
+ */
+public class PretreatmentDeleteAPIDemo {
+    /** 产品密钥ID，产品标识 */
+    private final static String SECRETID = "your_secret_id";
+    /**
+     * 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
+     */
+    private final static String SECRETKEY = "your_secret_key";
+    /**
+     * 业务ID，易盾根据产品业务特点分配
+     */
+    private final static String BUSINESSID = "your_business_id";
+    /** 易盾反垃圾云服务忽略词删除接口地址 */
+    private final static String API_URL = "http://as.dun.163.com/v1/pretreatment/batchDelete";
+    /** 实例化HttpClient，发送http请求使用，可根据需要自行调参 */
+    private static HttpClient httpClient = HttpClient4Utils.createHttpClient(100, 20, 10000, 2000, 2000);
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        // 1.设置公共参数
+        params.put("secretId", SECRETID);
+        params.put("businessId", BUSINESSID);
+        params.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        params.put("nonce", String.valueOf(new Random().nextInt()));
+        params.put("signatureMethod", "MD5"); // MD5, SM3, SHA1, SHA256
+        params.put("version", "v1");
+
+        // 2.设置私有参数
+        Set<String> idSet = new HashSet<>();
+        idSet.add("3641110");
+        idSet.add("3641111");
+
+        params.put("ids", String.join(",", idSet));
+        // 预处理参数
+        params = Utils.pretreatmentParams(params);
+        // 3.生成签名信息
+        String signature = SignatureUtils.genSignature(SECRETKEY, params);
+        params.put("signature", signature);
+
+        // 4.发送HTTP请求，这里使用的是HttpClient工具包，产品可自行选择自己熟悉的工具包发送请求
+        String response = HttpClient4Utils.sendPost(httpClient, API_URL, params, Consts.UTF_8);
+
+        // 5.解析接口返回值
+        JsonObject resultObject = new JsonParser().parse(response).getAsJsonObject();
+        int code = resultObject.get("code").getAsInt();
+        String msg = resultObject.get("msg").getAsString();
+        if (code == 200) {
+            boolean result = resultObject.get("result").getAsBoolean();
+            System.out.println(String.format("忽略词删除返回 %s", result));
+        } else {
+            System.out.println(String.format("ERROR: code=%s, msg=%s", code, msg));
+        }
+    }
+}
