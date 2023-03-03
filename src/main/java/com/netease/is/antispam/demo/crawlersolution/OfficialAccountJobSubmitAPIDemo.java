8d8e13ae13379fda941d2cff2610034b126322cc
luozhigang <luozhigang@corp.netease.com>
2023-03-03 15:51:10 +0800

    demo补充
    (cherry picked from commit 2bd7ffa2923592e0dad5ab86232cc3f484fbc48d)

---
+++ src/main/java/com/netease/is/antispam/demo/crawlersolution/OfficialAccountJobSubmitAPIDemo.java
@@ -0,0 +1,86 @@
+/*
+ * @(#) CrawlerSubmitAPIDemo.java 2020-04-23
+ *
+ * Copyright 2020 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.netease.is.antispam.demo.crawlersolution;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.netease.is.antispam.demo.utils.HttpClient4Utils;
+import com.netease.is.antispam.demo.utils.SignatureUtils;
+import com.netease.is.antispam.demo.utils.Utils;
+import org.apache.http.Consts;
+import org.apache.http.client.HttpClient;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * 调用易盾反垃圾云服务网站检测解决方案提交接口API实例
+ *
+ * @author huangwu
+ * @version 2020-04-23
+ */
+public class OfficialAccountJobSubmitAPIDemo {
+    /** 产品密钥ID，产品标识 */
+    private final static String SECRETID = "your_secret_id";
+    /** 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露 */
+    private final static String SECRETKEY = "your_secret_key";
+    /** 易盾反垃圾云服务文档检测在线提交地址 */
+    private final static String API_URL = "http://as.dun.163.com/v1/official-accounts/job/submit";
+    /** 实例化HttpClient，发送http请求使用，可根据需要自行调参 */
+    private static HttpClient httpClient = HttpClient4Utils.createHttpClient(100, 20, 10000, 2000, 2000);
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        // 1.设置公共参数
+        params.put("secretId", SECRETID);
+        params.put("version", "v1.0");
+        params.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        params.put("nonce", String.valueOf(new Random().nextInt()));
+        // MD5, SM3, SHA1, SHA256
+        params.put("signatureMethod", "MD5");
+
+        // 2.设置私有参数
+        params.put("dataId", "6a7c754f9de34eb8bfdf03f209fcfc02");
+        params.put("wechatAccount", "123456");
+        params.put("officialAccountName", "xxx公众号");
+        params.put("strategy", "3");
+        params.put("maxCheckCount", "100");
+        params.put("type", "4");
+        params.put("startTime", String.valueOf(System.currentTimeMillis()));
+        params.put("endTime", String.valueOf(System.currentTimeMillis()));
+        // 多个检测项时用英文逗号分隔
+        params.put("checkFlags", "1,2");
+        params.put("callbackUrl", "http://xxx.com");
+
+        // 预处理参数
+        params = Utils.pretreatmentParams(params);
+        // 3.生成签名信息
+        String signature = SignatureUtils.genSignature(SECRETKEY, params);
+        params.put("signature", signature);
+
+        // 4.发送HTTP请求，这里使用的是HttpClient工具包，产品可自行选择自己熟悉的工具包发送请求
+        String response = HttpClient4Utils.sendPost(httpClient, API_URL, params, Consts.UTF_8);
+
+        // 5.解析接口返回值
+        JsonObject jObject = new JsonParser().parse(response).getAsJsonObject();
+        int code = jObject.get("code").getAsInt();
+        String msg = jObject.get("msg").getAsString();
+        if (code == 200) {
+            JsonObject result = jObject.getAsJsonObject("result");
+            System.out.println(String.format("SUCCESS: taskId=%s, dataId=%s", result.get("taskId").getAsString(),
+                    result.get("dataId").getAsString()));
+        } else {
+            System.out.println(String.format("ERROR: code=%s, msg=%s", code, msg));
+        }
+    }
+}
---
+++ src/main/java/com/netease/is/antispam/demo/crawlersolution/WeiBoCallbackReceiveDemo.java
@@ -0,0 +1,74 @@
+/*
+ * @(#) CrawlerResourceCallbackReceiveController.java 2023-03-03
+ *
+ * Copyright 2023 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.netease.is.antispam.demo.crawlersolution;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+
+import static com.netease.is.antispam.demo.utils.SignatureUtils.verifySignature;
+
+/**
+ * 微博检测任务结果获取-推送
+ */
+@RestController
+public class WeiBoCallbackReceiveDemo {
+
+    /**
+     * 产品密钥ID，产品标识
+     */
+    private static final String SECRETID = "your_secret_id";
+    /**
+     * 产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
+     */
+    private static final String SECRETKEY = "your_secret_key";
+    /**
+     * 业务ID，易盾根据产品业务特点分配
+     */
+    private static final String BUSINESSID = "your_business_id";
+
+    /**
+     * 图片回调数据接收接口demo
+     *
+     * @param request
+     * @throws UnsupportedEncodingException
+     */
+    @PostMapping(value = "/weiBo/callback/receive")
+    public void weiBoCallbackReceive(HttpServletRequest request) throws UnsupportedEncodingException {
+        boolean verifyFlag = verifySignature(request, SECRETID, SECRETKEY, BUSINESSID);
+        if (!verifyFlag) {
+            throw new RuntimeException("signature verify failed");
+        }
+        String callbackData = request.getParameter("callbackData");
+        JsonObject resultObject = new JsonParser().parse(callbackData).getAsJsonObject();
+
+        JsonArray resultArray = resultObject.getAsJsonArray("result");
+        if (null == resultArray || resultArray.size() == 0) {
+            System.out.println("暂时没有结果需要获取，请稍后重试！");
+        } else {
+            for (JsonElement jsonElement : resultArray) {
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+                JsonObject machineCheckResult = jsonObject.getAsJsonObject("antispam");
+                if (machineCheckResult != null) {
+                    System.out.printf("机器检测结果:%s%n", machineCheckResult);
+                }
+                JsonObject censorResult = jsonObject.getAsJsonObject("censor");
+                if (censorResult != null) {
+                    System.out.printf("人工审核结果:%s%n", censorResult);
+                }
+            }
+        }
+
+    }
+
+}
